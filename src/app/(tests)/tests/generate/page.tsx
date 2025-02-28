d79e0eca732b36e368645d668de5d076d3498978
Denis Gukov <denis.guk@konomic.com>
2025-02-28 23:47:05 +0300

    first-stage: added gpt requests, added api

--- src/app/(tests)/tests/generate/page.tsx
+++ src/app/(tests)/tests/generate/page.tsx
@@ -6,6 +6,17 @@ import { ETEST_STEPS } from '@/app/(tests)/tests/generate/interfaces';
 import CustomButton from '@/components/ui/button/CustomButton';
 import { useRouter } from 'next/navigation';
 import TestsPrepareLayout from '@/app/(tests)/tests/components/TestsPrepareLayout';
+import {
+  Button,
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Field,
+  Input,
+  Label,
+} from '@headlessui/react';
+import Api from '@/core/api/api';
 
 const TestsGenerate = () => {
   const [step, setStep] = useState(ETEST_STEPS.FIRST);
@@ -13,6 +24,15 @@ const TestsGenerate = () => {
   const [level, setLevel] = useState<number>();
   const [questionsAmount, setQuestionsAmount] = useState<number>();
   const router = useRouter();
+  const [password, setPassword] = useState('');
+
+  const [passwordModal, setPasswordModal] = useState(false);
+
+  const handleSetPassword = (e: string) => {
+    console.log(e);
+
+    setPassword(e);
+  };
 
   const checkNextStep = useMemo(() => {
     if (step === ETEST_STEPS.FIRST) {
@@ -31,7 +51,7 @@ const TestsGenerate = () => {
       if (step !== ETEST_STEPS.THIRD) {
         setStep(step + 1);
       } else {
-        router.push('/tests/generate');
+        setPasswordModal(true);
       }
     } else {
       if (step !== ETEST_STEPS.FIRST) {
@@ -40,6 +60,19 @@ const TestsGenerate = () => {
         router.push('/tests');
       }
     }
+  };
+
+  const generateTests = async () => {
+    const data = {
+      password,
+      amount: questionsAmount,
+      level,
+      spec,
+    };
+
+    const result = await Api.post('/gpt/generate', data);
+
+    console.log(result);
   };
 
   let stepMarkup = null;
@@ -137,6 +170,62 @@ const TestsGenerate = () => {
           />
         </div>
       </div>
+      <Dialog
+        open={passwordModal}
+        as={'div'}
+        className={'relative z-10 focus:outline-none'}
+        onClose={() => setPasswordModal(false)}
+      >
+        <div className={'fixed inset-0 z-10 w-screen overflow-y-auto'}>
+          <div className={'flex min-h-full items-center justify-center p-4'}>
+            <DialogPanel
+              transition
+              className={
+                'w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0'
+              }
+            >
+              <DialogTitle
+                as={'h3'}
+                className={'text-base/7 font-medium text-white'}
+              >
+                Введите пароль для генерации
+              </DialogTitle>
+              <p className={'mt-2 text-sm/6 text-white/50'}>
+                Введите временный пароль для генерации теста. Пароль можно
+                посмотреть в документации к сервису.
+              </p>
+              <div className={'w-full max-w-md mt-3'}>
+                <Field>
+                  <Label className={'text-sm/6 font-medium text-white'}>
+                    Пароль
+                  </Label>
+                  <Input
+                    className={
+                      'mt-1 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-black focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                    }
+                    value={password}
+                    onInput={(val) =>
+                      handleSetPassword(
+                        (val.target as unknown as { value: string }).value
+                      )
+                    }
+                  />
+                </Field>
+              </div>
+              <div className={'mt-4'}>
+                <Button
+                  className={
+                    'inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700'
+                  }
+                  onClick={generateTests}
+                >
+                  Сгенерировать
+                </Button>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
     </TestsPrepareLayout>
   );
 };
