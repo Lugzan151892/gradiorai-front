e64d776c635354eb0e605f5ee000c220da3d7d1f
Denis Gukov <denis.guk@konomic.com>
2025-02-28 21:37:08 +0300

    first-stage: removed static config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  /** Все ниже настройки для статики */
-  output: 'export', // Включает статический экспорт
-  trailingSlash: true, // Добавляет слеш в конце URL (если нужно)
-  images: {
-    unoptimized: true, // Отключает Next.js Image Optimization (требует сервера)
-  },
 };
 
 export default nextConfig;
