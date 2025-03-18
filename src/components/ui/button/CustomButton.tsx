9e4f2d15b5fb34f605e7aa05baa4e4515af9a42a
Denis Gukov <denis.gukov@allio.ru>
2025-03-18 16:39:17 +0300

    fixes: fixed buttons, fixed colors

--- src/components/ui/button/CustomButton.tsx
+++ src/components/ui/button/CustomButton.tsx
@@ -1,13 +1,13 @@
 import { Button } from '@headlessui/react';
 import React from 'react';
-import CustomIcon from '@/components/ui/icon/CustomIcon';
 
 interface ICustomButtonProps {
   text?: string;
   className?: string;
   children?: Readonly<React.ReactNode>;
-  type?: 'success' | 'error' | 'warning' | 'forward' | 'back';
+  type?: 'success' | 'error' | 'warning';
   level?: 1 | 2 | 3;
+  color?: 'white';
   onClick?: () => void;
   disabled?: boolean;
   maxRounded?: boolean;
@@ -27,25 +27,31 @@ const CustomButton: React.FC<ICustomButtonProps> = ({
   selected,
   small,
   fullWidth,
+  color
 }) => {
   const disabledClass = disabled ? 'pointer-events-none opacity-40 !bg-gray-second' : '';
   const selectedClass = selected ? 'border-success' : 'border-transparent';
-  const sizeClasses = small ? 'px-2 py-1' : `px-4 py-2 ${fullWidth ? 'w-full' : ''} text-2xl`;
+  const sizeClasses = small ? 'px-2 py-1 text-base' : `px-4 py-2 ${fullWidth ? 'w-full' : ''} text-2xl`;
   const getTypeClasses = () => {
-    switch (type) {
-      case 'success':
-        return 'bg-success text-white';
-      case 'error':
-        return 'bg-error text-white';
-      case 'warning':
-        return 'bg-warning text-white';
-      case 'forward':
-        return 'bg-main-blue text-white';
-      case 'back':
-        return 'bg-main-blue text-white';
-      default:
-        return 'bg-main-blue text-white';
+    if (type) {
+      switch (type) {
+        case 'success':
+          return 'bg-success text-white data-[hover]:bg-white data-[hover]:text-black data-[hover]:border-success';
+        case 'error':
+          return 'bg-error text-white';
+        case 'warning':
+          return 'bg-warning text-white';
+      }
     }
+
+    if (color) {
+      switch (color) {
+        case 'white':
+          return 'bg-white text-black data-[hover]:bg-gray data-[hover]:border-white';
+      }
+    }
+
+    return 'bg-main-blue text-white';
   };
 
   return (
@@ -53,25 +59,7 @@ const CustomButton: React.FC<ICustomButtonProps> = ({
       className={`${sizeClasses} ${(maxRounded && 'rounded-xl') || 'rounded-lg'} ${disabledClass} ${getTypeClasses()} ${className || ''} ${selectedClass} border-2`}
       onClick={onClick}
     >
-      {type === 'back' || type === 'forward' ? (
-        <div className={'w-10 h-10 flex items-center justify-center bg-white rounded-full'}>
-          <div className={'text-main-blue text-3xl font-bold h-max'}>
-            {type === 'back' ? (
-              <CustomIcon
-                name={'arrow-left'}
-                color={disabled ? 'var(--second-gray)' : 'var(--main-blue)'}
-              />
-            ) : (
-              <CustomIcon
-                name={'arrow-right'}
-                color={disabled ? 'var(--second-gray)' : 'var(--main-blue)'}
-              />
-            )}
-          </div>
-        </div>
-      ) : (
-        children || text
-      )}
+      {children || text}
     </Button>
   );
 };
