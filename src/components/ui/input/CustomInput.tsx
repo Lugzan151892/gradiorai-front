415a495dc36017a145783d488829e04dc6a8df69
Denis Gukov <denis.guk@konomic.com>
2025-03-22 20:04:03 +0300

    login: fixed login and registration pages

--- src/components/ui/input/CustomInput.tsx
+++ src/components/ui/input/CustomInput.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { Field, Input, Label } from '@headlessui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import CustomIcon from '@/components/ui/icon/CustomIcon';
 
 interface ICustomInputProps {
   label?: string;
   placeholder?: string;
   value?: string | number;
-  error?: string;
-  validation?: boolean;
+  error?: string[] | string;
+  icon?: 'password' | 'email';
+  type?: 'text' | 'password';
   className?: string;
   onInput?: (val: string) => void;
   onChange?: (val: string) => void;
@@ -19,12 +20,15 @@ const CustomInput: React.FC<ICustomInputProps> = ({
   label,
   placeholder,
   value = '',
+  type = 'text',
+  icon,
   error,
-  validation,
   className,
   onInput,
   onChange,
 }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [currentInputType, setCurrentInputType] = useState<'text' | 'password'>(type);
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (onInput) {
       onInput(e.target.value);
@@ -35,26 +39,73 @@ const CustomInput: React.FC<ICustomInputProps> = ({
       onChange(e.target.value);
     }
   };
-  const classes = error && validation ? 'border-error' : 'border-gray';
-  const errorTextClasses = error && validation ? '' : 'opacity-100';
+  const handleSetType = () => {
+    if (currentInputType === 'password') {
+      setCurrentInputType('text');
+    } else {
+      setCurrentInputType('password');
+    }
+  };
+
+  const errorsList = Array.isArray(error) ? error : [error];
+  const classes = error && errorsList?.length ? 'border-error' : 'border-gray';
   return (
     <div className={'w-full ' + (className || '')}>
-      <Field>
+      <Field
+        className={'relative'}
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
         {label ? <Label className={'text-xl mb-1 text-black text-nowrap'}>{label}</Label> : null}
-        <div className={'flex bg-white w-full rounded-lg py-1.5 px-3 text-black border-2 ' + classes}>
-          <CustomIcon name={'password'} />
+        <div
+          className={'flex bg-white w-full rounded-input pl-5 pr-3 py-3 text-black border-2 ' + classes}
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}
+        >
+          {icon ? (
+            <CustomIcon
+              className={'mr-2'}
+              color={'var(--text-gray)'}
+              name={icon}
+            />
+          ) : null}
           <Input
             value={value}
             placeholder={placeholder}
             onInput={handleInput}
             onChange={handleChange}
+            type={currentInputType}
             className={
-              'w-full pl-3 border-l-2 text-sm/6 focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 ' +
+              `w-full pl-3 ${icon ? 'border-l-2' : ''} text-base focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 ` +
               classes
             }
           />
+          {type === 'password' ? (
+            <CustomIcon
+              className={'ml-1'}
+              color={'var(--text-gray)'}
+              name={currentInputType === 'password' ? 'eye-closed' : 'eye-opened'}
+              onClick={handleSetType}
+            />
+          ) : null}
         </div>
-        {validation ? <div className={'text-error text-xs h-5 pt-1' + errorTextClasses}>{error}</div> : ''}
+        {error && errorsList?.length && showTooltip && (
+          <div className={'absolute left-4 top-14 mt-1 bg-white text-red-600 shadow-lg rounded text-sm w-64 p-2 z-10'}>
+            <div className={'absolute -top-2 left-3 w-3 h-3 bg-white transform rotate-45'} />
+            {errorsList.map((error, index) => (
+              <div
+                key={index}
+                className={'flex items-center gap-2'}
+              >
+                <CustomIcon
+                  name={'warning'}
+                  size={15}
+                />
+                <span className={'text-xs'}>{error}</span>
+              </div>
+            ))}
+          </div>
+        )}
       </Field>
     </div>
   );
