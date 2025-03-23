d60488e6e14cc5c2ea9e4a2b443ef6d5c53d80c6
Denis Gukov <denis.guk@konomic.com>
2025-03-23 19:01:41 +0300

    login: fixed errorhandler

--- src/core/utils/error/errorHandler.ts
+++ src/core/utils/error/errorHandler.ts
@@ -1,27 +1,15 @@
-import { IResponse } from '@/core/api/interfaces';
+import { IResponseError } from '@/core/api/interfaces';
 import { AppDispatch } from '@/store';
 import { openModal } from '@/store/tech/techSlice';
 
-const errorHandler = (error: IResponse<null>, dispatch: AppDispatch) => {
-  if (error?.errorMessage) {
-    dispatch(
-      openModal({
-        text: error?.errorMessage,
-        type: 'error',
-        status: error.status,
-      })
-    );
-  } else {
-    dispatch(
-      openModal({
-        text:
-          error?.message ||
-          'Произошла ошибка.\r\nСвяжитесь с разработчиками. asdas',
-        type: 'error',
-        status: error?.status || 500,
-      })
-    );
-  }
+const errorHandler = (error: IResponseError, dispatch: AppDispatch) => {
+  dispatch(
+    openModal({
+      text: error?.payload?.message || 'Произошла ошибка.\r\nСвяжитесь с разработчиками. asdas',
+      type: 'error',
+      status: error?.payload?.statusCode || 500,
+    })
+  );
 };
 
 export default errorHandler;
