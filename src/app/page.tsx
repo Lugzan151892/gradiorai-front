185d990cf141c84d3b55afa59618b4ca87e3f0ea
Denis Gukov <denis.gukov@allio.ru>
2025-04-01 10:52:48 +0300

    main-page: fixed main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,32 +17,47 @@ const Home = () => {
     router.push('/login');
   };
   return (
-    <div className={'w-full h-full bg-[url("../assets/images/auth-bg.png")] bg-no-repeat bg-cover flex flex-col bg-left"'}>
+    <div className={'min-w-full min-h-full bg-[url("../assets/images/auth-bg.png")] bg-no-repeat bg-cover flex flex-col bg-left"'}>
       <div className={"desktop:px-10 mobile:px-5 mobile:pt-3 pt-12 flex"}>
-        <div className={"text-white text-6xl"}>
+        <div className={"text-white text-6xl mobile:text-5xl hover:text-low-green cursor-pointer"}>
           Gradior
         </div>
         {
           !user?.id &&
-          <div className={'ml-auto flex gap-2'}>
-            <CustomIcon
-              className={'cursor-pointer'}
-              name={'user'}
-              size={36}
-              color={'var(--main-white)'}
-              caption={'Войти'}
-              onClick={handleLogin}
-            />
-          </div>
+          <>
+            <div className={'ml-auto flex gap-2 desktop:hidden'}>
+              <CustomIcon
+                className={'cursor-pointer'}
+                name={'user'}
+                size={30}
+                color={'var(--main-white)'}
+                caption={'Войти'}
+                onClick={handleLogin}
+                />
+            </div>
+            <div className={'ml-auto flex gap-2 mobile:hidden'}>
+              <CustomIcon
+                className={'cursor-pointer'}
+                name={'user'}
+                size={36}
+                color={'var(--main-white)'}
+                caption={'Войти'}
+                onClick={handleLogin}
+                />
+            </div>
+          </>
         }
       </div>
-      <div className={"desktop:my-auto mobile:h-full mobile:mt-16 desktop:grid desktop:grid-cols-2 desktop:gap-x-2 desktop:grid-rows-[max-content,max-content] mobile:flex mobile:flex-col mobile:items-center desktop:ml-36 mobile:px-5"}>
+      <div className={"desktop:my-auto mobile:h-full mobile:mt-20 desktop:grid desktop:grid-cols-2 desktop:gap-x-2 desktop:grid-rows-[max-content,max-content] mobile:flex mobile:flex-col mobile:items-center desktop:ml-36 mobile:px-5"}>
         <div className={"text-white max-w-[690px]"}>
           <div className={"desktop:text-5xl mobile:text-3xl"}>
             AI Тестирование
           </div>
-          <div className={"desktop:text-2xl mobile:text-xl mt-4"}>
+          <div className={"text-2xl mobile:hidden mt-4"}>
             От новичка до эксперта - наши AI-тесты помогут вам оценить уровень знаний и найти точки роста. Выбирайте направление, отвечайте на вопросы, развивайтесь. Прокачивайте навыки с нами!
+          </div>
+          <div className={"text-xl desktop:hidden mt-4"}>
+            От новичка до эксперта - наши AI-тесты помогут вам оценить уровень знаний и найти точки роста. 
           </div>
         </div>
         <div className={"max-w-[540px] mobile:w-full desktop:row-span-2 desktop:mt-16 mobile:mt-5 desktop:mr-4"}>
@@ -61,8 +76,8 @@ const Home = () => {
           />
         </div>
         <div className={"grow desktop:hidden"} />
-        <div className={"text-white desktop:mt-36 mobile:mb-5 desktop:max-w-[450px]"}>
-          <div className={"desktop:hidden text-2xl"}>
+        <div className={"text-white desktop:mt-36 mobile:mb-5 mobile:mt-5 desktop:max-w-[450px]"}>
+          <div className={"desktop:hidden text-xl"}>
             Начните прямо сейчас!
           </div>
           <div className={"mobile:hidden text-2xl"}>
