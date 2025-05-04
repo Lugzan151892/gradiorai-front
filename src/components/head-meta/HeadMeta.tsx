6ccd2fdaab83bf7ab2cd931bfc491d4d04131f1c
Denis Gukov <denis.guk@konomic.com>
2025-05-04 14:20:42 +0300

    final: added seo component, added generate modal

---
+++ src/components/head-meta/HeadMeta.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import React from 'react';
+
+interface IHeadMetaProps {
+  title: string;
+  description: string;
+  url?: string;
+}
+
+const HeadMeta: React.FC<Readonly<IHeadMetaProps>> = ({ title, description, url }) => (
+  <Head>
+    <title>{title}</title>
+    <meta
+      name={'description'}
+      content={description}
+    />
+    <meta
+      property={'og:title'}
+      content={title}
+    />
+    <meta
+      property={'og:description'}
+      content={description}
+    />
+    <meta
+      property={'og:url'}
+      content={url || 'https://gradiorai.ru/'}
+    />
+    <meta
+      name={'twitter:card'}
+      content={'summary_large_image'}
+    />
+  </Head>
+);
+
+export default HeadMeta;
