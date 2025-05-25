570ffdbeb6efe549af377cc94759e8af51920a84
Denis Gukov <denis.guk@konomic.com>
2025-05-25 14:51:56 +0300

    interview: added interview gpt settings

--- src/app/(views)/system/layout.tsx
+++ src/app/(views)/system/layout.tsx
@@ -5,7 +5,7 @@ const SystemLayout: React.FC<{
   children: Readonly<React.ReactNode>;
 }> = ({ children }) => {
   return (
-    <div className={'grid grid-cols-1 grid-rows-layout h-full overflow-hidden'}>
+    <div className={'w-full h-full flex flex-col'}>
       <AppHeader />
       <div className={'flex justify-center w-full h-full overflow-y-auto'}>
         <div className={'w-full'}>{children}</div>
