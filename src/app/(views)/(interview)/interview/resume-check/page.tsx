268f80142eab71db299d151500388c70a1857e8a
Denis Gukov <denis.guk@konomic.com>
2025-06-03 22:42:41 +0300

    resume: added check resume page, fixed types

---
+++ src/app/(views)/(interview)/interview/resume-check/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import FileDropzone from '@/components/ui/file-dropzone/FileDropzone';
+import ScrollContainer from '@/components/ui/scrollarea/CustomScrollarea';
+import React, { useState } from 'react';
+import AuthConfirmButton from '@/app/(views)/(auth)/components/AuthConfirmButton';
+import { setLoading } from '@/features/loading/loadingSlice';
+import errorHandler from '@/core/utils/error/errorHandler';
+import { useAppDispatch } from '@/hooks/redux';
+import Api from '@/core/api/api';
+import InterviewMessage from '../[id]/components/InterviewMessage';
+
+const ResumePrepare = () => {
+  const [userCV, setUserCV] = useState<null | File>(null);
+  const [checkResult, setCheckResult] = useState('');
+  const dispatch = useAppDispatch();
+
+  const checkResume = async () => {
+    if (!userCV) {
+      return;
+    }
+
+    try {
+      dispatch(setLoading(true));
+      const resultFiles = [];
+      if (userCV) {
+        resultFiles.push(userCV);
+      }
+
+      const result = await Api.postFormData<{ cv: File | null }, { result: string }>('/interview/test-resume', {
+        cv: userCV,
+      });
+
+      setCheckResult(result.payload.result);
+    } catch (e) {
+      errorHandler(e, dispatch);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  return (
+    <div className={'w-full max-w-[1360px] mx-auto flex flex-col'}>
+      <h1 className={'text-5xl mb-2'}>Проверка резюме</h1>
+      <h2 className={'text-xl'}>Приложите свое резюме и мы дадим рекомендации по его исправлению</h2>
+      <div className={'bg-bg-transparent-25 rounded-10 p-4 mt-9 flex-grow mb-9 overflow-hidden'}>
+        <ScrollContainer>
+          <div className={'flex flex-col w-full h-full justify-center'}>
+            <div className={'flex w-full justify-around'}>
+              <div className={'max-w-md'}>
+                <div className={'mb-5 text-3xl'}>Ваше резюме</div>
+                <FileDropzone
+                  maxFileSize={2}
+                  formats={['docx', 'pdf']}
+                  onFileSelected={setUserCV}
+                />
+                {userCV ? <div>{userCV.name}</div> : null}
+              </div>
+              <div className={'max-w-md'}>
+                <div className={'mb-5 text-3xl'}>Результат проверки</div>
+                {checkResult && <InterviewMessage message={checkResult} />}
+              </div>
+            </div>
+          </div>
+        </ScrollContainer>
+      </div>
+      <div className={'w-full flex items-center mt-2 mb-4'}>
+        <AuthConfirmButton
+          className={'!w-[170px] mx-auto'}
+          customBorder
+          size={24}
+          icon={'check'}
+          disabled={!!checkResult}
+          text={'Проверить'}
+          onClick={checkResume}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ResumePrepare;
