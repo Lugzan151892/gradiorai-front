7fdee15afea19b844ba8870430cc1d43cccf3a4e
Denis Gukov <denis.guk@konomic.com>
2025-06-19 15:25:53 +0300

    new-design: removed extra icons

--- src/components/ui/icon/utils/index.ts
+++ src/components/ui/icon/utils/index.ts
@@ -2,27 +2,13 @@ import IconMarkup from '@/components/ui/icon/utils/IconMarkup';
 
 const getIconViewBox = (name: keyof typeof IconMarkup) => {
   switch (name) {
-    case 'arrow-left':
-      return '0 0 22 16';
-    case 'arrow-right':
-      return '0 0 26 23';
     case 'cross':
       return `0 -0.5 25 25`;
-    case 'login':
-      return '0 0 32 32';
-    case 'refresh':
-      return '0 0 21 21';
-    case 'reload':
-      return '0 0 16 16';
     case 'star':
     case 'star-transparent':
       return '0 0 44 44';
-    case 'sand-clock':
-      return '0 0 19 21';
     case 'user-login':
       return '0 0 25 25';
-    case 'open-password':
-      return '0 0 14 21';
     case 'wallet':
       return '0 0 30 30';
     case 'turn-off':
