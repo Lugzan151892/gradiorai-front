c12ba35215579ca7648d90dd0fd7053fb68d3ca4
Denis Gukov <denis.guk@konomic.com>
2025-08-10 15:54:42 +0300

    profile: fixed cv file issues, fixed file input, fixed results mobile

--- src/core/interfaces/types.ts
+++ src/core/interfaces/types.ts
@@ -22,6 +22,7 @@ export interface IUser {
     question_id: number;
     user_id: number;
   }>;
+  files: Array<IFile>;
 }
 
 export interface IUserReview {
@@ -113,4 +114,5 @@ export interface IFile {
   path: string;
   public: boolean;
   createdAt: string;
+  type: string;
 }
